8a920af2d292614cab5deacec4edda94c2aa6bf7
mnsndr <pmeenakshisundaram05@gmail.com>
2017-03-07 23:02:06 +0530

    Android Project for Temple
    Initial

---
+++ app/src/main/java/com/shop/thirupathi/MainActivity.java
@@ -0,0 +1,29 @@
+package com.shop.thirupathi;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    /**CONSTANTS**/
+    private static final String TAG = MainActivity.class.getCanonicalName();
+
+    /**MEMBERS**/
+    private WebView  mWebView;
+    private ProgressBar mLoading;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView)findViewById(R.id.wvPortal);
+        mLoading = (ProgressBar) findViewById(R.id.pbLoading);
+        mWebView.loadUrl("file:///android_asset/www/index.html");
+        WebSettings mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+    }
+
+}
